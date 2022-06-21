1ce4bd915fbdcb1fd79a770b59dc3c6eeef8fba6
coker <goocng@gmail.com>
2022-06-21 09:07:37 -0700

    reactjs.calculator

--- src/App.js
+++ src/App.js
@@ -3,22 +3,41 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+ <div class="container">
+	<div class="row col3">
+		<div class="col-md-12"><h1 class="text-right">0</h1></div>
+	</div>
+	<div class="row" >
+		<div class="col-md-3 col1"><h1>AC</h1></div>
+		<div class="col-md-3 col1"><h1>+/-</h1></div>
+		<div class="col-md-3 col1"><h1>%</h1></div>
+		<div class="col-md-3 col2"><h1>÷</h1></div>
+	</div>
+	<div class="row">
+		<div class="col-md-3 col1"><h1>7</h1></div>
+		<div class="col-md-3 col1"><h1>8</h1></div>
+		<div class="col-md-3 col1"><h1>9</h1></div>
+		<div class="col-md-3 col2"><h1>×</h1></div>
+	</div>
+	<div class="row">
+		<div class="col-md-3 col1"><h1>4</h1></div>
+		<div class="col-md-3 col1"><h1>5</h1></div>
+		<div class="col-md-3 col1"><h1>6</h1></div>
+		<div class="col-md-3 col2"><h1>-</h1></div>
+	</div>
+	<div class="row">
+		<div class="col-md-3 col1"><h1>1</h1></div>
+		<div class="col-md-3 col1"><h1>2</h1></div>
+		<div class="col-md-3 col1"><h1>3</h1></div>
+		<div class="col-md-3 col2"><h1>+</h1></div>
+	</div>
+	
+     <div class="row">
+     	<div class="col-md-6 col1"><h1>0</h1></div>
+     	<div class="col-md-3 col1"><h1>.</h1></div>
+     	<div class="col-md-3 col2"><h1>=</h1></div>
+     </div>
+</div>
   );
 }
 
